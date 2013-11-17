aec815154e2ea8e54232f2d77b28bb27e5fe0911
abhishek <abhishek@ubuntu.(none)>
2013-11-17 17:40:09 -0500

    Completing PostProcessDot script

--- useful_scripts/PostProcessDot.py
+++ useful_scripts/PostProcessDot.py
@@ -4,9 +4,48 @@ import os
 dot1Values = []
 dot2Values = []
 
+def enclosed(helix1_orig, helix2_orig):
+	
+	helix1 = map(int, helix1_orig)
+	helix2 = map(int, helix2_orig)
+
+	if(helix1[0] > helix2[0] and helix1[1] < helix2[1]):
+		#test for enclosing
+		i = helix2[2]
+		for x in range(i):
+			if(helix1[0]-i == helix2[0] and helix1[1]+i == helix2[1]):
+				return True
+	if(helix2[0] > helix1[0] and helix2[1] < helix1[1]):
+		i = helix1[2]
+		for x in range(i):
+			if(helix2[0]-i == helix1[0] and helix2[1]+i == helix1[1]):
+				return True
+	return False
+
+
 def compareDotValues():
-	print dot1Values
-	print dot2Values
+	result_strict = 0.0
+	result_moderate = 0.0
+
+	for helix1 in dot1Values:
+		for helix2 in dot2Values:
+			if(helix1[0] == helix2[0] and helix1[1] == helix2[1] and helix1[2] == helix2[2]):
+				result_strict += 1
+				result_moderate += 1
+				print helix1
+				print helix2
+				print ("+1\n")
+				break
+			elif(enclosed(helix1, helix2)):
+				result_moderate += 0.5
+				print helix1
+				print helix2
+				print ("+0.5\n")
+				break
+				
+	print ("\n")
+	print (">>Strict Score: %f" %(result_strict/len(dot1Values)))
+	print (">>Mod Score: %f" %(result_moderate/len(dot1Values)))
 
 def postProcess(inputFile_1, inputFile_2):
 	cmd = "cat "+ inputFile_1 + " | grep tr | grep -v legend | sed -e 's#<tr><td>.*</td><td>\(.*\)</td><td>\(.*\)</td></tr>#\\1 \\2#g' > dot_1.txt" 
@@ -46,6 +85,8 @@ def main():
 	inputFile_1 = sys.argv[1]
 	inputFile_2 = sys.argv[2]
 	postProcess(inputFile_1, inputFile_2)
+	os.system("rm dot_1.txt")
+	os.system("rm dot_2.txt")
 
 if __name__ =="__main__":
 	main();
